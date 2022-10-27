023c9f1f2df37cbbe9f5c5422706f560cff4e492
dylanToews <dylan.toews@yahoo.ca>
2022-10-26 18:14:13 -0600

    function built to search through arrays for specified words. Searches horizontally and vertically. Worked on with Will Wadman

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+
+  if (letters.length <= 0) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  let verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalJoin.push([]);
+    for (let j = 0; j < letters.length; j++) {
+      verticalJoin[i].push(letters[j][i]);
     }
-}
+  }
+
+  verticalJoin = verticalJoin.map(ls => ls.join(''));
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (j of verticalJoin) {
+    if (j.includes(word)) return true;
+  }
+
+  return false;
+
+};
 
 module.exports = wordSearch
+
